f9ae956c6b70f222f8c257dd6e748855abdc6355
anjiladhikari <anjiladlee10@gmail.com>
2023-08-04 20:44:36 +0545

    css added in each component

--- FirstOne/pizza-menu/src/index.js
+++ FirstOne/pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,27 +58,31 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast react Pizza Co.</h1>;
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
       <Pizza />
-    </div>
+    </main>
   );
 }
 
 function Footer() {
-  return <footer>We're currently open</footer>;
+  return <footer className="footer">We're currently open</footer>;
 }
 
 function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2> Pizza Spinaci </h2>
+      <h3> Pizza Spinaci </h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
