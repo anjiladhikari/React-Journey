1f01f538284528c6475acd0029ee3350072cf2a7
anjiladhikari <anjiladlee10@gmail.com>
2023-08-05 15:40:55 +0545

    tertiary operator in footer

--- FirstOne/pizza-menu/src/index.js
+++ FirstOne/pizza-menu/src/index.js
@@ -80,7 +80,27 @@ function Menu() {
 }
 
 function Footer() {
-  return <footer className="footer">We're currently open</footer>;
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <div className="order">
+          <p>
+            We're open until {closeHour}:00. Come visit us or order online.{" "}
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          We are happy to welcome you between {openHour}:00 and {closeHour}:00{" "}
+        </p>
+      )}
+    </footer>
+  );
 }
 
 function Pizza(props) {
