b4998e7a44d464faec7620af4c7b04c3f9558a5f
anjiladhikari <anjiladlee10@gmail.com>
2023-08-05 21:29:10 +0545

    conditional in element

--- FirstOne/pizza-menu/src/index.js
+++ FirstOne/pizza-menu/src/index.js
@@ -132,7 +132,7 @@ function Pizza({pizzaObj}) {
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <h3> {pizzaObj.name}</h3>
       <p>{pizzaObj.ingredients}</p>
-      <span>{pizzaObj.price}</span>
+      <span>{pizzaObj.soldOut? "Sold Out": pizzaObj.price}</span>
     </li>
   );
 }
