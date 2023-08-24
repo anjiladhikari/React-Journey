fc566aa7b2e8acebb84a24a6f647eb1e5805e81c
anjiladhikari <anjiladlee10@gmail.com>
2023-08-24 21:15:01 +0545

    items added

--- SecondOne/travel-list/src/App.js
+++ SecondOne/travel-list/src/App.js
@@ -1,5 +1,12 @@
 import "./index.css";
 
+const initialItems = [
+  { id: 1, description: " Passports", quantity: 2, packed: false },
+  { id: 2, description: " Socks", quantity: 12, packed: false },
+  { id: 3, description: " Charger", quantity: 1, packed: true },
+
+];
+
 function App() {
   return (
     <div className="App">
@@ -23,8 +30,29 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: "line-through"}:{}} >
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
