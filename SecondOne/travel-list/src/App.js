b4f8024115a35badcf5fc71dc2761df922df266c
anjiladhikari <anjiladlee10@gmail.com>
2023-09-02 12:06:36 +0545

    drived state stat

--- SecondOne/travel-list/src/App.js
+++ SecondOne/travel-list/src/App.js
@@ -29,7 +29,7 @@ function App() {
         onDeleteItems={handleDeleteItems}
         onToggleItems={handleToggleItems}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -111,10 +111,25 @@ function Item({ item, onDeleteItems, onToggleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀 </em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You have got everything! ready to go ✈️"
+          : ` 🧳 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
