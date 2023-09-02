d1f355407b80b31b13eeefe849f717402ce57e29
anjiladhikari <anjiladlee10@gmail.com>
2023-09-02 22:39:47 +0545

    managed folder structure

---
+++ SecondOne/travel-list/src/components/App.js
@@ -0,0 +1,47 @@
+import Form from "./Form";
+import Logo from "./Logo";
+import  PackingList  from "./PackingList";
+import  Stats  from "./Stats";
+import "../index.css";
+import { useState } from "react";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItems(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleReset() {
+    const confirmed = window.confirm("Do you want to clear list??");
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="App">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItems={handleDeleteItems}
+        onToggleItems={handleToggleItems}
+        onHandleRest={handleReset}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+export default App;
