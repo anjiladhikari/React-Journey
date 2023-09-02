1230002e26a66fc797dcac7ef471f9f67d830a91
anjiladhikari <anjiladlee10@gmail.com>
2023-09-02 15:21:44 +0545

    sorting us added

--- SecondOne/travel-list/src/App.js
+++ SecondOne/travel-list/src/App.js
@@ -78,6 +78,8 @@ function Form({ onAddItems }) {
   );
 }
 function PackingList({ items, onDeleteItems, onToggleItems }) {
+  const [sortBy, setSortBy] = useState("input");
+
   return (
     <div className="list">
       <ul>
@@ -90,6 +92,14 @@ function PackingList({ items, onDeleteItems, onToggleItems }) {
           />
         ))}
       </ul>
+
+      <div className="action">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input"> Sort by input order</option>
+          <option value="description"> Sort by description</option>
+          <option value="packed"> Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
