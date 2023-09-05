9ab3e48b6567ed9e069b7e151d2d57946537b38d
anjiladhikari <anjiladlee10@gmail.com>
2023-09-05 19:58:11 +0545

    more changes

--- Exercise/tipcalculator/src/App.js
+++ Exercise/tipcalculator/src/App.js
@@ -1,22 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <Bill />
-
-      <Mysat />
-
-      <Friendsat />
-
-
-
-
+      <TipCalculator />
     </div>
   );
 }
 
-function Bill() {
+function TipCalculator() {
+  const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+  const tip = ((percentage1 + percentage2) / 200) * bill;
+
+  function handleReset() {
+    setBill("");
+    setPercentage1(0);
+    setPercentage2(0);
+  }
+
+  return (
+    <div>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentage ></SelectPercentage>
+      <SelectPercentage></SelectPercentage>
+    </div>
+  );
+}
+
+function BillInput({ bill, onSetBill }) {
   return (
     <div>
       <h2>How much was the bill ? </h2>
@@ -25,31 +39,15 @@ function Bill() {
   );
 }
 
-function Mysat() {
+function SelectPercentage({ myTips, handleMyTips }) {
   return (
     <>
-      <h2>How did you like the service ? </h2>
-
+      <h2>How did you like the service ?</h2>
       <select>
-        <option value="">Dissatisfied (0%)</option>
-        <option value="">It was okay (5%)</option>
-        <option value="">It was good (10%)</option>
-        <option value="">Absoultely amazing (20%)</option>
-      </select>
-    </>
-  );
-}
-
-function Friendsat() {
-  return (
-    <>
-      <h2>How did your friend like the service? </h2>
-
-      <select>
-        <option value="">Dissatisfied (0%)</option>
-        <option value="">It was okay (5%)</option>
-        <option value="">It was good (10%)</option>
-        <option value="">Absoultely amazing (20%)</option>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absoultely amazing (20%)</option>
       </select>
     </>
   );
