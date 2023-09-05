d9a4e03b26958c550b8a96e08fe2d28aa4fe727c
anjiladhikari <anjiladlee10@gmail.com>
2023-09-05 08:58:39 +0545

    start tipcalc

---
+++ Exercise/tipcalculator/src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+
+function App() {
+  return (
+ 
+
+<>
+
+
+<Bill/>
+
+<Mysat/>
+
+<Friendsat/>
+
+
+</>
+
+
+  );
+}
+
+
+function Bill (){}
+
+
+function Mysat(){}
+
+function Friendsat(){}
+
+export default App;
