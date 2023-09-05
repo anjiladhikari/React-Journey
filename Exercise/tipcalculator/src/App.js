42e5bc4480d2b0da8146a56bdf35a3647da8d852
anjiladhikari <anjiladlee10@gmail.com>
2023-09-05 09:55:52 +0545

    static site

--- Exercise/tipcalculator/src/App.js
+++ Exercise/tipcalculator/src/App.js
@@ -1,32 +1,54 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
- 
+    <>
+      <Bill />
 
-<>
+      <Mysat />
 
-
-<Bill/>
-
-<Mysat/>
-
-<Friendsat/>
-
-
-</>
-
-
+      <Friendsat />
+    </>
   );
 }
 
+function Bill() {
+  return (
+    <div>
+      <h2>How much was the bill ? </h2>
+      <input type="number" placeholder="Bill value" />
+    </div>
+  );
+}
 
-function Bill (){}
+function Mysat() {
+  return (
+    <>
+      <h2>How did you like the service ? </h2>
 
+      <select>
+        <option value="">Dissatisfied (0%)</option>
+        <option value="">It was okay (5%)</option>
+        <option value="">It was good (10%)</option>
+        <option value="">Absoultely amazing (20%)</option>
+      </select>
+    </>
+  );
+}
 
-function Mysat(){}
+function Friendsat() {
+  return (
+    <>
+      <h2>How did your friend like the service? </h2>
 
-function Friendsat(){}
+      <select>
+        <option value="">Dissatisfied (0%)</option>
+        <option value="">It was okay (5%)</option>
+        <option value="">It was good (10%)</option>
+        <option value="">Absoultely amazing (20%)</option>
+      </select>
+    </>
+  );
+}
 
 export default App;
