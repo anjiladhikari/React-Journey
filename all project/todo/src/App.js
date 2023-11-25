05bdc647a434e9d7c6bee11d29183ae558612158
anjiladhikari <anjiladlee10@gmail.com>
2023-11-25 20:13:35 +0545

    starting

---
+++ all project/todo/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+
+      <h1>REACT TODO APP</h1>
+
+      <form>
+        <input type='text' name='todo' placeholder='Create a new todo' />
+        <button>Add</button>
+
+      </form>
+
+    </div>
+  );
+}
+
+
