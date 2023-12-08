1c1885e15c2379a653f67ebf6bafb7123c9cb649
anjiladhikari <anjiladlee10@gmail.com>
2023-12-08 21:21:06 +0545

    button added

--- all project/seemore/src/App.js
+++ all project/seemore/src/App.js
@@ -37,16 +37,27 @@ export default function App() {
 }
 
 function TextExpander(
-  collapsedNumWords,
-  expandButtonText,
-  collapseButtonText,
-  buttonColor,
-  expanded,
-  children
+  { collapsedNumWords = 10,
+    expandButtonText = "Show more",
+    collapseButtonText = "Show less",
+    buttonColor = "#1f09cd",
+    expanded = false,
+    className,
+    children }
 ) {
   const [isExpanded, setIsExpanded] = useState(expanded);
 
-  const displayText =isExpanded ? children:children
+  const displayText = isExpanded ? children : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
+  ;
+
+  const buttonStyle = {
+    background: "none",
+    border: "none",
+    font: "inherit",
+    cursor: "pointer",
+    marginLeft: "6px",
+    color: buttonColor
+  };
 
 
 
@@ -58,6 +69,7 @@ function TextExpander(
 
 
       <span>{displayText}</span>
+      <button  onClick={()=> setIsExpanded((exp)=>!exp)} style={buttonStyle}>{isExpanded? collapseButtonText:expandButtonText}</button>
 
     </div>
   )
