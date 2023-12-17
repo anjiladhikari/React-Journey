cff4d11e838da307d4c5eef343cac91ec00a73b1
anjiladhikari <anjiladlee10@gmail.com>
2023-12-17 21:04:28 +0545

    taking input

--- projects/5.0-curr_conversion/src/App.js
+++ projects/5.0-curr_conversion/src/App.js
@@ -1,19 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 export default function App() {
+
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+
+
+
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+
+      <select
+        value={fromCur}
+        onChange={(e) => setFromCur(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+
+      <select
+        value={toCur}
+        onChange={(e) => setToCur(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
