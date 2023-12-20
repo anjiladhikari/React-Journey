3bf404b26f71fa1cc90cbb125253cc044c748cb5
anjiladhikari <anjiladlee10@gmail.com>
2023-12-20 20:39:09 +0545

    added header.js

---
+++ projects/7.0-quiz/src/Header.js
@@ -0,0 +1,23 @@
+
+
+
+function Header() {
+
+
+    return (
+
+
+        <header className="app-header">
+
+            <img src="logo512.png" alt="react" />
+
+            <h1>The React Quiz</h1>
+
+
+
+        </header>
+    )
+
+}
+
+export default Header;
