770bee50a5b17f0c129a56c7353b4a860392128a
anjiladhikari <anjiladlee10@gmail.com>
2023-12-21 14:29:57 +0545

    state management

--- projects/7.0-quiz/src/App.js
+++ projects/7.0-quiz/src/App.js
@@ -1,9 +1,61 @@
 import './App.css';
 import Header from './Header';
 import Main from './Main'
+import { useEffect, useReducer } from 'react';
+
+
+
+const initialState = {
+  questions: [],
+  status: "loading"
+}
+
+function reducer(state, action) {
+
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready"
+      }
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error"
+      }
+
+    default:
+      throw new Error("Action unknown")
+
+  }
+}
 
 
 function App() {
+
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+
+  useEffect(function () {
+
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatchEvent({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatchEvent({ type: "dataFailed" }));
+
+
+  }, [])
+
+
+
+
+
+
+
+
   return (
     <div className="App">
       <Header />
